eaefc1db2cd988e3509751a9426f0a12ae508042
Liza Levitis <elizabeth.levitis@mail.mcgill.ca>
2020-11-05 16:47:20 -0500

    ran group-level freesurfer 7.1.1 analysis

---
+++ freesurfer_batch_group.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+#SBATCH --time=3:00:00 
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=4G 
+
+module load singularity 
+
+singularity run \
+--bind /data/DNU \
+/data/DNU/liza/singularity_imgs/freesurfer_v7.1.1.sif \
+/data/DNU/liza/data/XXY/Nifti \
+/data/DNU/liza/data/XXY/Nifti/derivatives/Freesurfer \
+group2 \
+--participant_label $(xargs --arg-file=subs_with_t1w.txt echo) \
+--license_file "/data/DNU/liza/license.txt" \
+--skip_bids_validator
+
+
+# /usr/local/apps/freesurfer/7.1.1/license.txt

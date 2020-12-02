3f90e0db28c12f6c51be94f2bdbb048dec08e9ec
Liza Levitis <elizabeth.levitis@mail.mcgill.ca>
2020-12-02 11:05:49 -0500

    adding T1w space to fmriprep command

--- fmriprep_participant.sh
+++ fmriprep_participant.sh
@@ -12,9 +12,10 @@ export SINGULARITYENV_FS_LICENSE=/data/DNU/liza/license.txt
 # extract subjects
 SUBJECT=$( sed "${SLURM_ARRAY_TASK_ID}q;d" /data/DNU/liza/data/XXY/demo_data/XXY_3TC_PseudoGUIDs.txt )
 
-fmriprep Nifti Nifti/derivatives/fMRIPrep participant --participant_label ${SUBJECT} \
+fmriprep Nifti Nifti/derivatives/ participant --participant_label ${SUBJECT} \
 --notrack \
 --nthreads 16 \
 --omp-nthreads 16 \
 --fs-subjects-dir Nifti/derivatives/Freesurfer \
+--output-spaces T1w MNI152NLin2009cAsym \
 --skip_bids_validation

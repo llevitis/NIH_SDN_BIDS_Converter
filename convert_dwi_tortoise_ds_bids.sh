cd7fceb5ab854750460649e9786a51064b77bdc8
Liza Levitis <elizabeth.levitis@mail.mcgill.ca>
2021-05-19 00:36:08 -0400

    fixed the DS ImportDICOM call

--- convert_dwi_tortoise_ds_bids.sh
+++ convert_dwi_tortoise_ds_bids.sh
@@ -14,16 +14,23 @@ bids_dir=$5
 
 dwi_dicom_sub=${dwi_dicom_dir}/sub-${sub}
 dicom_readme=${dicom_dir}/sub-${sub}/ses-${ses}/*/README-Study.txt
-g01_num=`cat ${dicom_readme} | grep "edti_cdiflist08_g01" | cut -d "," -f 3 | cut -d ":" -f 2`
-g02_num=`cat ${dicom_readme} | grep "edti_cdiflist08_g02" | cut -d "," -f 3 | cut -d ":" -f 2`
-g03_num=`cat ${dicom_readme} | grep "edti_cdiflist08_g03" | cut -d "," -f 3 | cut -d ":" -f 2`
+g01_num=`cat ${dicom_readme} | grep "edti_cdiflist08_g01" | cut -d " " -f 3 | cut -d ":" -f 2`
+g02_num=`cat ${dicom_readme} | grep "edti_cdiflist08_g02" | cut -d " " -f 3 | cut -d ":" -f 2`
+g03_num=`cat ${dicom_readme} | grep "edti_cdiflist08_g03" | cut -d " " -f 3 | cut -d ":" -f 2`
 g01_dicom_dir=${dicom_dir}/sub-${sub}/ses-${ses}/*/${g01_num}
 g02_dicom_dir=${dicom_dir}/sub-${sub}/ses-${ses}/*/${g02_num}
 g03_dicom_dir=${dicom_dir}/sub-${sub}/ses-${ses}/*/${g03_num}
 gradients_file=${dwi_dicom_dir}/cdiflist08
 
-if [ ! -x $dwi_dicom_sub ]
+if [ -f $dicom_readme ]
 then
+    g01_num=`cat ${dicom_readme} | grep "edti_cdiflist08_g01" | cut -d "," -f 3 | cut -d ":" -f 2`
+    g02_num=`cat ${dicom_readme} | grep "edti_cdiflist08_g02" | cut -d "," -f 3 | cut -d ":" -f 2`
+    g03_num=`cat ${dicom_readme} | grep "edti_cdiflist08_g03" | cut -d "," -f 3 | cut -d ":" -f 2`
+    g01_dicom_dir=${dicom_dir}/sub-${sub}/ses-${ses}/*/${g01_num}
+    g02_dicom_dir=${dicom_dir}/sub-${sub}/ses-${ses}/*/${g02_num}
+    g03_dicom_dir=${dicom_dir}/sub-${sub}/ses-${ses}/*/${g03_num}
+    gradients_file=${dwi_dicom_dir}/cdiflist08
     mkdir -p $dwi_dicom_sub
     mkdir -p $dwi_dicom_sub/dicom
     mkdir $dwi_dicom_sub/dicom/g01_dicom_dir
@@ -32,7 +39,7 @@ then
     ln -s $g01_dicom_dir/* $dwi_dicom_sub/dicom/g01_dicom_dir
     ln -s $g02_dicom_dir/* $dwi_dicom_sub/dicom/g02_dicom_dir
     ln -s $g03_dicom_dir/* $dwi_dicom_sub/dicom/g03_dicom_dir
-    ln -s $gradients_file $dwi_dicom_sub/cdiflist09
+    ln -s $gradients_file $dwi_dicom_sub/cdiflist08
 
     # run tortoise --> get the mri_ap_proc and mri_pc_proc
     ImportDICOM -i $dwi_dicom_sub/dicom -g $dwi_dicom_sub/cdiflist08 -b 1100
@@ -56,5 +63,5 @@ then
     cp $dwi_dicom_sub/dicom_proc/dicom.nii.gz $bids_dir/sub-${sub}/ses-${ses}/dwi/sub-${sub}_ses-${ses}_dwi.nii.gz
     echo "New Nifti, bvec, and bval files have been copied over"
 else
-    echo "Subject directory for sub-${sub} already exists in dwi_dicom"
+    echo "sub-${sub} has a differently named README file directory"
 fi
